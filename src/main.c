ec1cbc02bfaf19d63965639db17f7c241067f7cc
codemuch <eric@codemuch.net>
2022-09-26 16:48:17 -0700

    working version

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include <json-c/json.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MOONCLI_URI "https://shrewdly.herokuapp.com/"
 #define MOONCLI_UA "mooncli (libcurl/7.85.0)"
@@ -22,24 +23,13 @@ size_t data_callback(char *buffer, size_t item_size, size_t item_ct, void *ignor
 size_t header_callback(char *buffer, size_t item_size, size_t item_ct, void *ignoreme);
 void print_http_status(CURL *curl);
 void request_failure(CURLcode *res);
+void json_pp(void);
 
 int main(void) {
 
     curl = curl_easy_init();
     CURLcode res;
     line_ct = 1;
-
-    struct json_object *parsed_json;
-    struct json_object *json_data;
-    struct json_object *json_metadata;
-    struct json_object *json_message;
-    struct json_object *json_description;
-    struct json_object *json_name;
-
-    struct Message {
-        char description[MOONCLI_MAX_DESC_LEN];
-        char name[MOONCLI_MAX_NAME_LEN];
-    } message;
 
     curl_easy_setopt(curl, CURLOPT_URL, MOONCLI_URI);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, MOONCLI_UA);
@@ -70,21 +60,28 @@ size_t data_callback(char *buffer, size_t item_size, size_t item_ct, void *ignor
     size_t byte_ct = item_size * item_ct;
 
     print_http_status(curl);
-    printf("[+] Chunk received: (%zu bytes)\n", byte_ct);
-    printf("%3d: ", line_ct++);
+    /* DEBUG PRINTS */
+    //printf("[+] Chunk received: (%zu bytes)\n", byte_ct);
+    //printf("%3d: ", line_ct++);
 
     /* Print the received chunk one byte at a time. */
-    for (char *p = buffer; p < &buffer[byte_ct]; ++p) {
+    for (char *p = buffer, *q = json_buffer; p < &buffer[byte_ct]; ++p) {
         if (*p != '\n') {
-            putchar(*p);
+            //putchar(*p);
+            if (q < &json_buffer[MOONCLI_MAX_BUFFER]) {
+                *q = *p;
+                q++;
+            }
         } else {
-            putchar(*p);
-            printf("%3d: ", line_ct);
+            /* DEBUG PRINTS */
+            //putchar(*p);
+            //printf("%3d: ", line_ct);
             ++line_ct;
         }
     }
 
-    printf("\n\n");
+    //printf("\n\n");
+    json_pp();
     return byte_ct;
 }
 
@@ -114,9 +111,37 @@ void print_http_status(CURL *curl) {
     long http_code = 0;
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
     
+    printf("[+] Status: ");
     switch (http_code) {
         case 200:
-            printf("[+] 200 OK\n");
+            printf("200 OK\n");
             break;
     }
+}
+
+void json_pp(void) {
+    struct json_object *parsed_json;
+    struct json_object *json_data;
+    struct json_object *json_metadata;
+    struct json_object *json_message;
+    struct json_object *json_description;
+    struct json_object *json_name;
+
+    struct Message {
+        char description[MOONCLI_MAX_DESC_LEN];
+        char name[MOONCLI_MAX_NAME_LEN];
+    } message;
+
+    parsed_json = json_tokener_parse(json_buffer);
+    json_object_object_get_ex(parsed_json, "data", &json_data);
+    json_object_object_get_ex(parsed_json, "meta", &json_metadata);
+    json_object_object_get_ex(json_data, "message", &json_message);
+    json_object_object_get_ex(json_message, "description", &json_description);
+    json_object_object_get_ex(json_message, "name", &json_name);
+
+    strncpy(message.description, json_object_get_string(json_description), MOONCLI_MAX_DESC_LEN);
+    strncpy(message.name, json_object_get_string(json_name), MOONCLI_MAX_NAME_LEN);
+
+    printf("Name: %s\n", message.name);
+    printf("Description: %s\n", message.description);
 }
